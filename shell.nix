9a2f3088bd5b3e92649d78876fc334c87a3ab005
Manuel Thalmann <m@nuth.ch>
2024-04-18 02:15:10 +0200

    Pin `nixpkgs` version

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/2fd19c8be2551a61c1ddc3d9f86d748f4db94f00";
   pkgs = import nixpkgs {
     overlays = [];
     config = {
