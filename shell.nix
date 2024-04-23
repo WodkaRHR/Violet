027783c8e95dd484ef843aa5d153ee10353d1cef
Manuel Thalmann <m@nuth.ch>
2024-04-23 23:56:50 +0200

    Update the `flake-compat` version

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
 (import (
   fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
-    sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5"; }
+    url = "https://github.com/edolstra/flake-compat/archive/0f9255e01c2351cc7d116c072cb317785dd33b33.tar.gz";
+    sha256 = "0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
+  }
 ) {
   src =  ./.;
 }).shellNix
